71f39e5dee517dc1730ad784742ba3501b077f1b
Angela Peng <apapangelapeng@gmail.com>
2023-01-06 17:57:55 -0500

    update make and split nullcline into its own file

--- cpp_code/nullcline.cpp
+++ cpp_code/nullcline.cpp
@@ -9,9 +9,12 @@
 
 using namespace std;
 
+/*
+THIS FILE IS JUST FOR ME TO TEST THINGS
+*/
 
-const char *path1 = "../data_files/reduced_V_output.csv";
-const char *path2 = "../data_files/reduced_null_output.csv";
+const char *path1 = "../data_files/nullcline_metadata.csv";
+const char *path2 = "../data_files/nullcline_output.csv";
 const char *path3 = "../data_files/reduced_diffusion_output.csv";
 
 vector<double> vec_V, vec_N, vec_Space,vec_tiny_N,vec_N_Zerodt,vec_V_Zerodt;
@@ -57,21 +60,42 @@ int reset_vecs(int x)
     return (0);
 }
 
+void output_metadata(double local_v_start,double local_v_end,double v_step,double local_n_start,double local_n_end,double n_step)
+{
+    ofstream create_file(path2);
+    ofstream myfile;
+    myfile.open(path2);
+    myfile<<"local_v_start,local_v_end,v_step,local_n_start,local_n_end,n_step \n";
+    myfile<<local_v_start<<",";
+    myfile<<local_v_end<<",";
+    myfile<<v_step<<",";
+    myfile<<local_n_start<<",";
+    myfile<<local_n_end<<",";
+    myfile<<n_step;
+    myfile.close();
+
+
+}
 double nullcline_generation(double x)
 {
     int y;
     reset_vecs(0);
+    
     double local_v_start = -0.15;
-    double local_n_start = -0.05;
-    double tiny_n_start = -0.05;
+    double local_v_end	=0.45;
+    double v_step	=0.0001;
+   double local_n_start = -0.05;
+    double local_n_end	= 0.12;
+    double n_step=0.0001;
+    output_metadata( local_v_start, local_v_end, v_step, local_n_start, local_n_end, n_step);
     vec_N.push_back(local_n_start);
     vec_V.push_back(local_v_start);
-    vec_tiny_N.push_back(local_n_start);
+
 
     double current = 0.0; 
 
     y = 0;
-    for (double v_temp = local_v_start; v_temp <= 0.45; v_temp += 0.0001)
+    for (double v_temp = local_v_start; v_temp <= local_v_end; v_temp += v_step)
     {
         // cout << "v temp : " << v_temp << endl;
         N_dt = e * (v_temp - (gam * vec_N[y]));
@@ -80,7 +104,7 @@ double nullcline_generation(double x)
     }
 
     y = 0;
-    for (double n_temp = local_n_start; n_temp <= 0.12; n_temp += 0.0001)
+    for (double n_temp = local_n_start; n_temp <= local_n_end; n_temp += n_step)
     {
         time_d = vec_V[y] * (vec_V[y] - v_threshold) * (v_max - vec_V[y]);
         V_dt = current + time_d - n_temp;
@@ -141,7 +165,7 @@ double output__nullcline_file(double x)
     ofstream myfile;
     myfile.open(path2);
 
-    nullcline_generation(0);
+   
 
     vector<int> sizes;
     /*cout<<vec_V.size()<<endl;
@@ -223,211 +247,13 @@ double output__nullcline_file(double x)
     return (x);
 }
 
-void write_Reduced_AP(vector<vector<double> > & v_map){
-    cout << "I HAVE BEEN SUMMONED " << endl;
-    ofstream create_file(path1);
-    ofstream myfile;
-    myfile.open(path1);
-
-    /*
-    for (V_start = -0.5; V_start <= v_range; V_start += 0.3)
-    {
-
-        string end = (V_start >= v_range - 0.3) ? "\n" : ",";
-        myfile << "V" << V_start << end;
-    }
-    */
-
-    myfile << "V" << V_start << "\n";
-    for (int i = 0; i < vec_V.size(); i++)
-    {
-        for (int j = 0; j < v_map.size(); j++)
-        {
-            string end = (j == v_map.size() - 1) ? "\n" : ",";
-            myfile << v_map[j][i] << end;
-        }
-
-        // cout << vec_V[i] << endl;
-    }
-
-    myfile << "\n";
-
-}
-
-double Reduced_AP(double z)
-{
-    reset_vecs(0);
-    int x = 0;
-
-    V_start = 0;
-
-    vec_V.push_back(V_start);
-    vec_N.push_back(N_start);
-
-    for (double i = 0; i <= 50; i += delta_t)
-    {
-        
-        if(i >= 5 && i <= 10){
-            current_temp = 0.05;
-        }
-        else{
-            current_temp = 0;
-        }
-        
-        cout << current_temp << endl; 
-
-        // cout << "Break Point 4" << endl;
-
-        time_d = vec_V[x] * (vec_V[x] - v_threshold) * (v_max - vec_V[x]);
-        V_dt = current_temp + time_d - vec_N[x];
-
-        cout << "V_DT " << V_dt << endl;
-        vec_V.push_back(vec_V[x] + V_dt);
-        N_dt = e * (vec_V[x] - (gam * vec_N[x]));
-        vec_N.push_back(vec_N[x] + N_dt);
-
-        // cout << "Break Point 5" << endl;
-
-        // cout << "x = " << x << endl;
-
-        x += 1;
-        }
-
-    vector<double> vec_V_deep = vec_V;
-    v_map_2d.push_back(vec_V_deep); 
-
-    cout << "V_start: " << V_start << endl;
-
-    write_Reduced_AP(v_map_2d);
-    return (0);
-}
-
-double Diffusion_AP(double z)
-{
-    ofstream create_file(path3);
-    ofstream myfile;
-    myfile.open(path3);
-
-    // cout << "Break Point 1" << endl;
-
-    double V_start = 0.4;
-    double N_start = 0;
-    double Space_start = 0;
-    double current = 0; 
-
-    int counter = 0;
-
-    // cout << "Break Point 3" << endl;
-
-    vec_V.push_back(V_start);
-    vec_N.push_back(N_start);
-    vec_Space.push_back(Space_start);
-
-    for (double i = 0; i <= 100; i += delta_t)
-    {
-        vec_Space.clear();
-
-        /*
-        if (i <= 2)
-        {
-            current_temp = current;
-        }
-        else
-        {
-            current_temp = 0;
-        }
-        */
-
-        time_d = vec_V[counter] * (vec_V[counter] - v_threshold) * (v_max - vec_V[counter]);
-        V_dt = current + time_d - vec_N[counter];
-        vec_V.push_back(vec_V[counter] + V_dt);
-        N_dt = e * (vec_V[counter] - (gam * vec_N[counter]));
-        vec_N.push_back(vec_N[counter] + N_dt);
-
-        /*
-        cout << "volt " << vec_V[counter] << endl;
-        cout << "time d = " << time_d << endl;
-        cout << "v_dt = " << V_dt << endl;
-        cout << "n " << vec_N[counter] << endl;
-        cout << "counter " << counter << endl;
-        */
-
-        counter += 1;
-
-        // cout << "Break Point 4" << endl;
-
-        // cout << i << ", and ROUNDED: " << floor(i) << endl;
-        if (counter % 10 == 0 || counter == 1)
-        {
-            //cout << "PASSED counter: " << counter << endl;
-            vec_Space.push_back(vec_V[counter]);
-            vec_Space.push_back(vec_V[counter]);
-            vec_Space.push_back(vec_V[counter]);
-            int local_counter = 2;
-            for (double space = 0; space <= x_range; space += delta_x)
-            {
-                // cout << "Voltage at this time: " << vec_V[counter] << endl;
-                // cout << "Break Point 5" << endl;
-
-                spatial_d = (vec_Space[local_counter] - (2 * vec_Space[local_counter - 1]) + vec_Space[local_counter - 2]);
-                vec_Space.push_back(diffusion * vec_Space[local_counter] - pow(delta_t, 2) * spatial_d);
-                local_counter += 1;
-                //cout << spatial_d << endl;
-                //cout << "counter = " << counter << endl; 
-            }
-            myfile << "T" << i << ",";
-            for (int i = 0; i < vec_Space.size(); i++)
-            {
-                myfile << vec_Space[i] << ",";
-                // cout << "Break Point 6" << endl;
-            }
-            myfile << "\n";
-        }
-
-        // cout << "Break point 6" << endl;
-
-        /*
-        myfile << "V" << diffusion << ",";
-        for(int i = 0; i < vec_V.size(); i++){
-            myfile << vec_V[i] << ",";
-            //cout << vec_V[i] << endl;
-        }
-        */
-
-        // cout << "Time: " << i << endl;
-    }
-    /*
-    myfile << "Voltage,";
-    for(int i = 0; i < vec_V.size(); i++){
-        myfile << vec_V[i] << ",";
-        //cout << vec_N[i] << endl;
-        //cout << "Break Point 6" << endl;
-    }
-    myfile << "\n";
-
-    myfile << "N,";
-    for(int i = 0; i < vec_N.size(); i++){
-        myfile << vec_N[i] << ",";
-        //cout << vec_N[i] << endl;
-        //cout << "Break Point 6" << endl;
-    }
-    */
-
-    myfile.close();
-
-    vector<double> vec_V_deep = vec_V;
-    v_map_2d.push_back(vec_V_deep); 
-    write_Reduced_AP(v_map_2d);
-
-    cout << "V_start: " << V_start << endl;
-    return (0);
-}
 
 int main(void)
 {
     cout << "Begin" << endl;
 
     //Reduced_AP(0);
+     nullcline_generation(0);
     output__nullcline_file(0);
     //Diffusion_AP(0);
 

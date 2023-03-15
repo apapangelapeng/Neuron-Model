95d3e59d8d927bd58c6743645a01e7210f344d33
jpowell24 <jrp24@sas.upenn.edu>
2023-03-15 14:44:11 -0400

    integrating HH AP

--- cpp_code/Piezo_Channel.cpp
+++ cpp_code/Piezo_Channel.cpp
@@ -14,7 +14,35 @@ THIS FILE IS JUST FOR ME TO TEST THINGS
 */
 
 const char *path1="../data_files/Piezo_Channel.csv";
+const char *path2="../data_files/static_wt_output.csv";
 
+double V_start = 0;
+double current;
+double V_temp;
+double Na_I_temp, K_I_temp, L_I_temp, HCN_I_temp, Cl_I_temp = 0;
+
+double a_n, b_n, a_h, b_h, a_m, b_m;
+double n_dynamic, m_dynamic, h_dynamic, n_inf, m_inf, h_inf;
+vector<double> vec_n, vec_m, vec_h;
+vector<double> vec_inf_n, vec_inf_m, vec_inf_h;
+vector<double> vec_tau_n, vec_tau_m, vec_tau_h;
+double tau_n, tau_m, tau_h;
+
+double V_dt; 
+vector<double> vec_V, vec_Na_I, vec_K_I, vec_L_I;
+vector<double> vec_Nap, vec_Kp;
+vector<double> vec_VWT;
+double C_m = 1; 
+
+double g_k = 36; 
+double g_Na = 120;
+double g_l = 0.3; 
+
+double E_k = -12; 
+double E_Na = 120; 
+double E_l = 10.6; 
+
+int static_ap_counter = 0; 
 
 //CONSTANTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 double log_convert = 2.303; // to convert from ln to log10
@@ -154,6 +182,160 @@ vector<double> vec_w;
 
 default_random_engine generator;
 normal_distribution<double> stochastic_opening(0,0.6);
+
+/*
+n is a kinetic equation built to track ONE kind of POTASSIUM channel's opening
+it will be a proportion of channels open, and or the activation of the channel
+potassium current can be generalized as: g*n^4*(V - E)
+Where V is resting voltage, E is membrane potential, and g is conductance
+m is the same, but for Na 
+h represents INACTIVATION of Na channels, so h and m compete
+The formulas are taken from page 37 of the Electrophysiology textbook
+*/
+
+double dynamical_h(double V){
+    //for some reason I decided to add in double h, so that we can store this value local to the method
+    //and, h_dynamic can be shared to main. I forget why I did this lol, I probably had something in mind
+    a_h = 0.07*exp(-V/20);
+    b_h = ((1)/(exp((30-V)/10) + 1));
+    h_inf = a_h/(a_h + b_h);
+    tau_h = 1/(a_h + b_h);
+    //the if statements are added to expunge NaN from the data
+    //storing the values in vectors so that they can be easily written to a csv file
+    vec_tau_h.push_back(tau_h);
+    vec_inf_h.push_back(h_inf);
+    return(0);
+}
+
+double dynamical_n(double V){
+    a_n = 0.01*((10-V)/(exp((10-V)/10) - 1));
+    if(V == 10){
+        a_n = 0.1; // This is the Taylor approx value for when divide by 0
+    }
+    b_n = 0.125*exp(-V/80);
+    n_inf = a_n/(a_n + b_n);
+    tau_n = 1/(a_n + b_n);
+    //cout << tau_n << endl;
+    vec_tau_n.push_back(tau_n);
+    vec_inf_n.push_back(n_inf);
+    //cout << V << endl;
+    return (0);
+}
+
+double dynamical_m(double V){
+    a_m = 0.1*((25 - V)/(exp((25-V)/10) - 1));
+    if (V == 25){
+        a_m = 1; // This is the Taylor approx value for when divide by 0
+    }
+    b_m = 4*exp(-V/18);
+    m_inf = a_m/(a_m + b_m);
+    tau_m = 1/(a_m + b_m);
+    //cout << tau_m << endl;
+    vec_tau_m.push_back(tau_m);
+    vec_inf_m.push_back(m_inf);
+    return (0);
+}
+
+
+double Static_WT_AP(double Ca_input){
+
+    //vec_Cl_I.push_back(0);
+
+    
+        current = Ca_input;
+        //cout << x << endl; 
+
+        if(static_ap_counter >= 1000){
+          current = 5;
+        }
+
+        //cout << "Break point 1" << endl;
+
+        dynamical_m(vec_V[static_ap_counter]);
+        dynamical_h(vec_V[static_ap_counter]);
+        dynamical_n(vec_V[static_ap_counter]);
+
+        //cout << "Break point 2" << endl;
+
+        vec_n.push_back(vec_n[static_ap_counter] + delta_T*((vec_inf_n[static_ap_counter] - vec_n[static_ap_counter])/vec_tau_n[static_ap_counter]));
+        vec_m.push_back(vec_m[static_ap_counter] + delta_T*((vec_inf_m[static_ap_counter] - vec_m[static_ap_counter])/vec_tau_m[static_ap_counter]));
+        vec_h.push_back(vec_h[static_ap_counter] + delta_T*((vec_inf_h[static_ap_counter] - vec_h[static_ap_counter])/vec_tau_h[static_ap_counter]));
+
+        //cout << "Break point 3" << endl;
+
+        K_I_temp = (g_k*pow(vec_n[static_ap_counter+1],4)*((vec_V[static_ap_counter]) - E_k));
+        Na_I_temp = (g_Na*pow(vec_m[static_ap_counter+1],3)*pow(vec_h[static_ap_counter+1],1)*((vec_V[static_ap_counter]) - E_Na));
+        L_I_temp = (g_l*((vec_V[static_ap_counter]) - E_l));
+
+        V_dt = (current - K_I_temp - Na_I_temp - L_I_temp)/C_m;
+        vec_V.push_back(vec_V[static_ap_counter] + delta_T*V_dt);
+
+        vec_VWT.push_back(vec_V[static_ap_counter] + delta_T*V_dt);
+
+        vec_Na_I.push_back(Na_I_temp);
+        vec_K_I.push_back(K_I_temp);
+        vec_L_I.push_back(L_I_temp);
+
+        //cout << V << endl;
+        //cout << V_temp << endl;
+        static_ap_counter++; 
+        //cout << x << endl;
+    return(0);
+}
+
+double output_WT_Static_AP(double x)
+{
+    ofstream create_file(path2);
+    ofstream myfile;
+    myfile.open(path2);
+
+    vector<int> sizes;
+    /*cout<<vec_V.size()<<endl;
+    cout<<vec_N.size()<<endl;
+    cout<<vec_tiny_N.size()<<endl;*/
+    sizes.insert(sizes.begin(),vec_V.size());
+    sizes.insert(sizes.begin(),vec_K_I.size());
+    sizes.insert(sizes.begin(),vec_Na_I.size());
+    sizes.insert(sizes.begin(),vec_L_I.size());
+    sort(sizes.begin(), sizes.end());
+    int max_size = sizes.back();
+    
+    cout << max_size << endl;
+
+    bool V; 
+    bool K_I; 
+    bool Na_I; 
+    bool L_I; 
+    
+    //cout << "Break point 4" << endl;
+
+    myfile << "V,K_I,Na_I,L_I\n";
+    for (int i = 0; i < max_size; i++)
+    {
+        //cout << "Break point 5" << endl;
+        V = (vec_V.size() > i) ? true : false;
+        K_I = (vec_K_I.size() > i) ? true : false;
+        Na_I = (vec_Na_I.size() > i) ? true : false;
+        L_I = (vec_L_I.size() > i) ? true : false;
+
+        //cout << "Break point 6" << endl;
+
+        if(V) myfile << vec_V[i] <<"," ;
+        if(!V) myfile <<"," ;
+        if(K_I) myfile << vec_K_I[i] << ",";
+        if(!K_I) myfile << ",";
+        if(Na_I) myfile << vec_Na_I[i] << ",";
+        if(!Na_I) myfile <<",";
+        if(L_I) myfile << vec_L_I[i];
+
+        //if(!dn_V_0) myfile << vec_tiny_N[i];
+
+        myfile << "\n";
+        //cout << "Break point 6" << endl;
+    }
+    myfile.close();
+    return (x);
+}
 
 double PotentialE(double out, double in, int Z) { //calculated in VOLTS NOT MILLIVOLTS
   double E = (R_constant * body_temp) / (F * Z) * log(out / in); // log(x) = ln(x) in cpp
@@ -313,6 +495,11 @@ double Piezo_Channel(double potential){
 }
 
 double Calcium_concentration(double time_range, double delta_T){
+  vec_V.push_back(V_start);
+  vec_n.push_back(dynamical_n(0));
+  vec_m.push_back(dynamical_m(0));
+  vec_h.push_back(dynamical_h(0));
+
   E_Ca = PotentialE(0.0024, 0.0000001, 2);
   vec_w.push_back(0);
   vec_num_open.push_back(0);
@@ -334,8 +521,8 @@ double Calcium_concentration(double time_range, double delta_T){
     Ca_c_dT = delta_T*(scaling_factor*Piezo_Channel(E_Ca) + scaling_factor*Compute_J_ryr(C_cyt) - scaling_factor*Compute_J_serca(C_cyt) + Compute_J_on(C_cyt));
     vec_Ca_conc.push_back(vec_Ca_conc[Ca_counter] + Ca_c_dT);
     Ca_counter++;
+    Static_WT_AP(vec_Ca_conc[Ca_counter]);
   }
-
   return(0);
 }
 
@@ -344,6 +531,7 @@ double voltage_output(double x)
     //reset_vecs(0);
     //Piezo_screen(1000, 10);
     Calcium_concentration(1000, delta_T);
+    output_WT_Static_AP(0);
 
     // for (int i = 0; i < 3; i++)
     // {

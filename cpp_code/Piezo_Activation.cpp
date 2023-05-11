2242597e66c753785d0e03df32bf59b668a6f6dc
jpowell24 <jrp24@sas.upenn.edu>
2023-05-11 17:20:13 -0400

    Piezo model 1 seems to work okay

--- cpp_code/Piezo_Activation.cpp
+++ cpp_code/Piezo_Activation.cpp
@@ -15,14 +15,21 @@ default_random_engine generator;
 // normal_distribution<double> stochastic_opening(0,4);
 
 
-normal_distribution<double> stiffness(0.7,0.1);
-normal_distribution<double> pressure(30,5);
+normal_distribution<double> stiffness(0.7,0.05);
+normal_distribution<double> pressure(5,5);
 normal_distribution<double> voltage(-70,10);
 
 vector<double> vec_P_Substrate;
 vector<double> vec_P_Pressure;
 vector<double> vec_P_Voltage;
 vector<double> vec_P_Total; 
+
+vector<double> vec_closed_inactive_local;
+vector<double> vec_closed_active_local;
+
+double open_local, closed_inactive_local, closed_active_local; 
+
+double local_N_Piezo = 100;
 
 double Piezo_P_Pressure(double i){
     double F_inf;
@@ -47,15 +54,15 @@ double Piezo_P_Voltage(double i){
 
 double Piezo_Channel(int time){
 
-    double stochastic_stiffness = stiffness(generator);
-    double stochastic_pressure = pressure(generator);
+    double stochastic_stiffness = abs(stiffness(generator));
+    double stochastic_pressure = abs(pressure(generator));
     double stochastic_voltage = voltage(generator);
 
+    if((time > 50) && (time < 60)){
+        stochastic_pressure = 30;
+    }
+
     //cout << stochastic_stiffness << " " << stochastic_pressure << " " << stochastic_voltage << endl;
-
-    int local_N_Piezo, open_local, closed_inactive_local, closed_active_local; 
-
-    local_N_Piezo = 100;
 
     double P_opening_temp;
     double Pressure_input, Substrate_input, Voltage_input; 
@@ -72,7 +79,13 @@ double Piezo_Channel(int time){
 
     P_opening_temp = 1/(exp((0.5 - P_total)/0.1) + 1);
 
-    open_local = vec_P_Total[time] + P_opening_temp*local_N_Piezo;
+    vec_closed_inactive_local.push_back(0.904837418*vec_closed_inactive_local[time] + (vec_P_Total[time] - 0.904837418*vec_P_Total[time]));
+    vec_closed_active_local.push_back((1 - P_opening_temp)*vec_closed_active_local[time] + (vec_closed_inactive_local[time] - 0.904837418*vec_closed_inactive_local[time]));
+
+    // 0.904837418 = e^-1/10
+
+    open_local = 0.904837418*vec_P_Total[time] + P_opening_temp*vec_closed_active_local[time];
+    // cout << open_local << " and " << P_opening_temp*vec_closed_active_local[time] << endl;
 
     // cout << P_opening_temp << endl;
     // open_local = vec_num_open[time] + P_opening_temp*closed_active_local;
@@ -92,9 +105,11 @@ double output_file(double x)
     ofstream myfile;
     myfile.open(path1);
 
-     vec_P_Total.push_back(0);
+    vec_P_Total.push_back(0);
+    vec_closed_inactive_local.push_back(0);
+    vec_closed_active_local.push_back(100);
 
-    for(double i = 0; i <= 10000; i++){
+    for(double i = 0; i <= 100; i++){
         Piezo_Channel(i);
     }
 
@@ -104,6 +119,8 @@ double output_file(double x)
     sizes.insert(sizes.begin(),vec_P_Pressure.size());
     sizes.insert(sizes.begin(),vec_P_Substrate.size());
     sizes.insert(sizes.begin(),vec_P_Voltage.size());
+    sizes.insert(sizes.begin(),vec_closed_active_local.size());
+    sizes.insert(sizes.begin(),vec_closed_inactive_local.size());
 
     sort(sizes.begin(), sizes.end());
     int max_size = sizes.back();
@@ -114,10 +131,12 @@ double output_file(double x)
     bool Bool_P_open2;
     bool Bool_P_open3;
     bool Bool_P_total;
+    bool Bool_P_active;
+    bool Bool_P_inactive;
 
     //cout << "Break point 4" << endl;
 
-    myfile << "Piezo,Pressure,Substrate,Voltage\n";
+    myfile << "Piezo_Open,Piezo_Closed,Piezo_Inactive,Pressure,Substrate,Voltage\n";
 
     for (int i = 0; i < max_size; i++)
     {
@@ -126,11 +145,17 @@ double output_file(double x)
         Bool_P_open1 = (vec_P_Pressure.size() > i) ? true : false;
         Bool_P_open2 = (vec_P_Substrate.size() > i) ? true : false;
         Bool_P_open3 = (vec_P_Voltage.size() > i) ? true : false;
+        Bool_P_active = (vec_closed_active_local.size() > i) ? true : false;
+        Bool_P_inactive = (vec_closed_inactive_local.size() > i) ? true : false;
 
         //cout << "Break point 6" << endl;
 
         if(Bool_P_total) myfile << vec_P_Total[i] << ",";
         if(!Bool_P_total) myfile <<",";
+        if(Bool_P_active) myfile << vec_closed_active_local[i] << ",";
+        if(!Bool_P_active) myfile <<",";
+        if(Bool_P_inactive) myfile << vec_closed_inactive_local[i] << ",";
+        if(!Bool_P_inactive) myfile <<",";
         if(Bool_P_open1) myfile << vec_P_Pressure[i] << ",";
         if(!Bool_P_open1) myfile <<",";
         if(Bool_P_open2) myfile << vec_P_Substrate[i] << ",";
